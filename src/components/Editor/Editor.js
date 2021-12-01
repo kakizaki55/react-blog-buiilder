e04434a922c06e8e6a42bb28148e9e8aa22ede1f
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-01 11:15:56 -0800

    got the font to work allright

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor({ title, setTitle, subtitle, setSubtitle }) {
+export default function Editor({ title, setTitle, setSubtitle, setFont }) {
   return (
     <div className="editor">
       <div className="form-control">
@@ -16,7 +16,7 @@ export default function Editor({ title, setTitle, subtitle, setSubtitle }) {
       </div>
 
       <div className="form-control">
-        <select>
+        <select onChange={(e) => setFont(e.target.value)}>
           <option value="architect">{"Architect's Daughter"}</option>
           <option value="comforter">Comforter</option>
           <option value="fredoka">Fredoka</option>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,12 +10,13 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title, subtitle }} />
-      <Editor {...{ title, setTitle, subtitle, setSubtitle }} />
+      <Preview {...{ title, subtitle, font }} />
+      <Editor {...{ setTitle, setSubtitle, setFont }} />
     </main>
   );
 }
